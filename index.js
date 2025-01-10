b50e09f69fbbf50dc4d7921b228a35a5ece2e231
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-01-10 11:47:17 -0800

    Part 2 completed

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const spaceRequired = (numWeeks, numPlants = 20, minSpaceRequired = 0.8) => {
 };
 
 console.log(
-  `20 plants will require ${spaceRequired(1)} square metres after 1 week`
+  `20 plants will require ${spaceRequired(1)} square meters after 1 week`
 );
 console.log(
-  `20 plants will require ${spaceRequired(2)} square metres after 2 weeks`
+  `20 plants will require ${spaceRequired(2)} square meters after 2 weeks`
 );
 console.log(
-  `20 plants will require ${spaceRequired(3)} square metres after 3 weeks`
+  `20 plants will require ${spaceRequired(3)} square meters after 3 weeks`
 );
 
 // Implement control flow to make decisions on whether the plants should be:
@@ -31,20 +31,39 @@ console.log(
 
 const PI = 3.1415;
 let radius = 5;
-const action = (numWeeks, area = PI * radius * radius) => {
-    let spaceNeeded = spaceRequired(numWeeks);
+const action = (numWeeks, area = PI * radius * radius, numPlants = 10) => {
+    let spaceNeeded = spaceRequired(numWeeks, numPlants);
     let capacity = spaceNeeded/area; // % of the garden that is taken up by the plants after given number of weeks
     if (capacity > 0 && capacity < .5) {
         return 'Plant more plants';
     } else if (capacity < 0.8) {
         return 'Monitor plant growth';
-    } else if (capacity > 0.8) {
+    } else if (capacity >= 0.8) {
         return 'Please prune plants';
     } else {
-        return 'Something went wrong';
+        return `Something went wrong, capacity is ${capacity * 100}%`;
     }
 };
 
 console.log(action(1));
 console.log(action(2));
 console.log(action(3));
+
+//============================= PART TWO ==================================
+// The conservation area in which the garden is located has multiple other gardens. 
+// Using the logic you have already created, determine:
+// The amount of additional space that would be required if the scientists were to start with 100 plants, and did not prune them for 10 weeks.
+
+// if we're pruning at 80% capacity, then the formula spaceNeeded = spaceRequired/0.8 will tell us how much space we need
+let spaceNeeded = spaceRequired(10, 100) / 0.8; //the space required after 10 weeks when starting with 100 plants
+
+let originalArea = PI * radius * radius;
+
+let additionalSpaceRqd = spaceNeeded - originalArea;
+
+console.log(`After 10 weeks, 100 plants will require ${spaceNeeded} square meters. This is ${additionalSpaceRqd} square meters more than the original space of ${originalArea} square meters`)
+// If the space remained circular, what would be the radius of this expanded garden?
+// the formula for this is sqrt(area/pi) = radius
+let newRadius = Math.sqrt(spaceNeeded/PI);
+
+console.log(`If the space remained circular the radius of the new garden would be ${newRadius} meters`);
